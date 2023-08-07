b94322bddd8a5ee1a7bc7ffaf72ad8830341fb26
PrymalInstynct <zimzamfam@gmail.com>
2023-08-06 19:54:03 -0600

    Adding lots of stuff to the repo

--- install_hyprland.sh
+++ install_hyprland.sh
@@ -37,7 +37,7 @@ pac() {
 
 # Utilities
 echo -e "\n${BLUE}###Installing Base Packages###${NC}\n"
-pac "base-devel sudo vim git cmake imv linux-headers noto-fonts-emoji openssh neofetch bashtop bat stow wget wl-clipboard otf-font-awesome podman buildah skopeo cni-plugins fuse-overlayfs slirp4netns man-db man-pages"
+pac "base-devel sudo vim git cmake imv linux-headers noto-fonts-emoji openssh neofetch btop bat stow wget wl-clipboard otf-font-awesome podman buildah skopeo cni-plugins fuse-overlayfs slirp4netns man-db man-pages"
 pac "bluez bluez-utils alsa-utils pipewire pipewire-alsa pipewire-pulse blueberry pavucontrol zip unzip unrar"
 
 # Paru, fonts and other AUR tools
@@ -51,7 +51,7 @@ sudo --user=$USER_NAME sh -c "paru -S --noconfirm --needed ttf-iosevka ttf-meslo
 
 # Sway & desktop tools
 echo -e "\n${BLUE}###Installing Hyprland###${NC}\n"
-sudo --user=$USER_NAME sh -c "paru -S --noconfirm --needed hyprland kitty waybar swaybg swaylock-effects wofi wlogout mako thunar ttf-jetbrains-mono-nerd noto-fonts-emoji polkit-gnome python-requests starship swappy grim slurp pamixer brightnessctl gvfs bluez bluez-utils lxappearance xfce4-settings dracula-gtk-theme dracula-icons-git xdg-desktop-portal-hyprland"
+sudo --user=$USER_NAME sh -c "paru -S --noconfirm --needed hyprland kitty waybar swaybg swaylock-effects swayidle wofi wlogout mako thunar ttf-jetbrains-mono-nerd noto-fonts-emoji polkit-gnome python-requests starship swappy grim slurp pamixer brightnessctl gvfs bluez bluez-utils lxappearance xfce4-settings dracula-gtk-theme dracula-icons-git xdg-desktop-portal-hyprland"
 
 # Environment
 echo "LIBSEAT_BACKEND=seatd" >> /etc/environment
